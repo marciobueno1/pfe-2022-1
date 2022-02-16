f9410496c0d83d6a01bc4d1cfdd212a284411697
Márcio Bueno <marcio.bueno@unicap.br>
2022-02-16 19:33:08 -0300

    Adicionado projeto react - app-01

---
+++ app-01/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      </header>
+      <h1>Alô Mundo!</h1>
+      <p>Novo parágrafo...</p>
+    </div>
+  );
+}
+
+export default App;
