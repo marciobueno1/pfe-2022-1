c87e201fe1db2fd90f01b36ab6d3900ec8c85ebd
Márcio Bueno <marcio.bueno@unicap.br>
2022-03-09 19:08:58 -0300

    Incluído props, state e useEffect (ciclo de vida)

--- app-01/src/App.js
+++ app-01/src/App.js
@@ -1,15 +1,37 @@
-import './App.css';
+import React from "react";
+import "./App.css";
+
+function ClockFuncional(props) {
+  const [date, setDate] = React.useState(new Date());
+  React.useEffect(() => {
+    console.log("componentDidMount");
+    const timerID = setInterval(() => {
+      setDate(new Date());
+    }, 1000);
+    return () => {
+      console.log("componentWillUnmount");
+      clearInterval(timerID);
+    };
+  }, []);
+  return (
+    <div>
+      <h1>Relógio: {props.nome}</h1>
+      <h2>It is {date.toLocaleTimeString()}.</h2>
+    </div>
+  );
+}
 
 function App() {
+  const [show, setShow] = React.useState(true);
+  function handleClick() {
+    setShow(!show);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-      <h1>Alô Mundo!</h1>
-      <p>Novo parágrafo...</p>
+    <div>
+      {show && <ClockFuncional nome="Primeiro Relógio" />}
+      <button onClick={handleClick}>Exibe/Oculta</button>
+      <ClockFuncional nome="Segundo Relógio" />
+      <ClockFuncional nome="Terceiro Relógio" />
     </div>
   );
 }
